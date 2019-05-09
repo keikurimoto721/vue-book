ad6ffd18af51dd1918f36f06f9123d97e43b5e23
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2019-05-09 23:09:49 +0900

    lesson4-3途中

---
+++ todo/todo.js
@@ -0,0 +1,24 @@
+new Vue({
+  el: "#app",
+  data: {
+    newtodo: "",
+    todolist: []
+  },
+  methods: {
+    addTodo: function() {
+      if (this.newtodo != "") {
+        this.todolist.push({
+          text: this.newtodo,
+          done: false,
+          hover: false
+        });
+        this.newtodo = "";
+      }
+    },
+    remove: function(index) {
+      if (this.todolist[index].done == true) {
+        this.todolist.splice(index, 1);
+      }
+    }
+  }
+});
