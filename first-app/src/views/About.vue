8663f4200b73d9fe804830f5a8947780b7c52ee1
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2019-05-16 19:16:49 +0900

    study vuex

--- first-app/src/views/About.vue
+++ first-app/src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>{{ storedMsg }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "about",
+  computed: {
+    storedMsg: function() {
+      return this.$store.state.message;
+    }
+  }
+};
+</script>
